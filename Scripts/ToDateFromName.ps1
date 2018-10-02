b4f04839a01617a749795863d83642243acef73d
Christopher Sprague <spragucm@gmail.com>
2018-10-02 19:32:56 +0200

    First prompt asks about the folder structure. If the structure uses the date then a second prompt asks about the date format. The source and destination are now checked for null and the source is checked to exist - prevents errors and misplacing of files.

--- Scripts/ToDateFromName.ps1
+++ Scripts/ToDateFromName.ps1
@@ -1,26 +1,42 @@
 #Determine the folder structure to create
-$choices = [System.Management.Automation.Host.ChoiceDescription[]](
-(New-Object System.Management.Automation.Host.ChoiceDescription "&DD-MM-YYYY","DD-MM-YYYY"),
-(New-Object System.Management.Automation.Host.ChoiceDescription "&MM-DD-YYYY","MM-DD-YYYY"))
+$structures = [System.Management.Automation.Host.ChoiceDescription[]](
+(New-Object System.Management.Automation.Host.ChoiceDescription "&Group by Date","Group by Date"),
+(New-Object System.Management.Automation.Host.ChoiceDescription "&Group by Year","Group by Year"),
+(New-Object System.Management.Automation.Host.ChoiceDescription "&Group by Year then Date","Group by Year then Date"),
+(New-Object System.Management.Automation.Host.ChoiceDescription "&Group by Year then Month","Group by Year then Month"),
+(New-Object System.Management.Automation.Host.ChoiceDescription "&Group by Year then Month then Day","Group by Year then Month then Day"))
+$Grouping = $host.ui.PromptForChoice('Bulk File Organizer', "Select Output Folder Structure", $structures, 2)
 
-$Answer = $host.ui.PromptForChoice('Folder Output Structure', "Question", $choices, 2)
-#Write-Output $Answer
+if($Grouping -eq 0 -or $Grouping -eq 2){
+#Determine the folder structure to create
+$formats = [System.Management.Automation.Host.ChoiceDescription[]](
+(New-Object System.Management.Automation.Host.ChoiceDescription "&DD-MM-YYYY","DD-MM-YYYY"),
+(New-Object System.Management.Automation.Host.ChoiceDescription "&MM-DD-YYYY","MM-DD-YYYY"),
+(New-Object System.Management.Automation.Host.ChoiceDescription "&YYYY-DD-MM","YYYY-DD-MM"),
+(New-Object System.Management.Automation.Host.ChoiceDescription "&YYYY-MM-DD","YYYY-MM-DD"))
+$Format = $host.ui.PromptForChoice('Bulk File Organizer', "Select Date Format", $formats, 0)
+}
 
 #Determine where the images are that need to be sorted
 $source = Read-Host "Enter Photos Source Path"
-#Write-Output $source
 
 #Determine where the images will be moved and sorted
 $destination = Read-Host "Enter Photos Destination Path"
-#Write-Output $destination
 
+#If the source or destination input was blank or the input paths don't exist then exit
+#No need to test if the destination exists since it will be created if it does not exist
+if(!($source) -or !($destination)){
+	Write-Host "Source and Destination cannot be null and must exist" -fore Yellow
+	exit
+}
+if(!(Test-Path $source)){
+	Write-Host "Failed to move. Source does not exist." -fore Yellow
+	exit
+}
 
 # Get the files which should be moved, without folders
 $files = Get-ChildItem $source -Recurse | where {!$_.PsIsContainer}
- 
-# List Files which will be moved
-#$files
- 
+  
 foreach ($file in $files)
 {
 # Get year and Month of the file using the filename
@@ -31,20 +47,32 @@ $month = $bn.substring(4,2)
 $day = $bn.substring(6,2)
 
 # Set Directory Path
-$Directory = $destination + "\" + $year + "\" + $day + "-" + $month + "-" + $year
 
-if($Answer -eq 1)
-{
-# Set Directory Path
-$Directory = $destination + "\" + $year + "\" + $month + "-" + $day + "-" + $year
+#Get date preferences if they exist - which is only when the option uses dates for folder names
+if(!($Format)){
+	#This catches null and empty format as well as format selection of 0
+	$Date = $day + "-" + $month + "-" + $year
+}elseif($Format -eq 1){
+	$Date = $month + "-" + $day + "-" + $year
+}elseif($Format -eq 2){
+	$Date = $year + "-" + $day + "-" + $month
+}elseif($Format -eq 3){
+	$Date = $year + "-" + $month + "-" + $day
 }
 
-# Out FileName, year and month
-#$file.Name
-#$year
-#$month
-#$day
- 
+#Create the full directory name using grouping and date preferences
+if($Grouping -eq 0){
+	$Directory = $destination + "\" + $Date
+}elseif($Grouping -eq 1){
+	$Directory = $destination + "\" + $year
+}elseif($Grouping -eq 2){
+	$Directory = $destination + "\" + $year + "\" + $Date
+}elseif($Grouping -eq 3){
+	$Directory = $destination + "\" + $year + "\" + $month
+}elseif($Grouping -eq 4){
+	$Directory = $destination + "\" + $year + "\" + $month + "\" + $day
+}
+
 # Create directory if it doesn't exsist
 if (!(Test-Path $Directory))
 {
